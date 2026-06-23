56ebfdfa5698c034460ad5293edcda10093fa270
achhara <ac6026010022@camtech.edu.kh>
2026-06-23 17:03:07 +0700

    Implement function to add a new student record

--- add.c
+++ add.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "student.h"
 
+void addStudent(Student students[], int *count) {
+    
+    if (*count >= MAX_STUDENTS) {
+        printf("\nError: Student database capacity reached (%d/%d).\n", *count, MAX_STUDENTS);
+        return;
+    }
+
+    Student newStudent;
+    printf("\n--- Add New Student Record ---\n");
+    printf("Enter Student ID: ");
+    scanf("%d", &newStudent.id);
+    
+  
+    for (int i = 0; i < *count; i++) {
+        if (students[i].id == newStudent.id) {
+            printf("Error: A student with ID %d already exists in the system.\n", newStudent.id);
+            return; 
+        }
+    }
+
+    printf("Enter Name: ");
+    getchar(); r
+    fgets(newStudent.name, NAME_MAX, stdin);
+    newStudent.name[strcspn(newStudent.name, "\n")] = 0; 
+
+   
+    printf("Enter Major: ");
+    fgets(newStudent.major, MAJOR_MAX, stdin);
+    newStudent.major[strcspn(newStudent.major, "\n")] = 0; 
+
+    printf("Enter Cumulative GPA: ");
+    scanf("%f", &newStudent.gpa);
+
+    students[*count] = newStudent;
+    
+    (*count)++;
+
+    printf("\nSuccess: Student record added successfully!\n");
+}
